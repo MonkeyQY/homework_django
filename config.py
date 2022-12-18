5a857498a0f035362eab3200a354ddcbee3ae6ec
monkey <vasilevskiy.job@gmail.com>
2022-12-18 09:57:05 +0400

    first commit

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+django_secret_key = os.getenv('DJANGO_SECRET_KEY')
+
+
+database_name = os.getenv('DATABASE_NAME', 'DjangoTestDB')
+database_user = os.getenv('DATABASE_USER', 'postgres')
+database_password = os.getenv('DATABASE_PASSWORD', 'admin')
+database_host = os.getenv('DATABASE_HOST', 'localhost')
+database_port = os.getenv('DATABASE_PORT', '5432')
